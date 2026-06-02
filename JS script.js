0dd6a1340edb5adc1dd9e723000202687cd514f6
EMILYYTALIAADOSS <emily.talia.santos@escola.pr.gov.br>
2026-06-02 10:35:24 -0300

    Update and rename README.md to JS script.js

---
+++ JS script.js
@@ -0,0 +1,71 @@
+//VERSÃO 1
+funtion funcao1()  {
+   var x = document.getE1ementById("i1").value;
+   var x = document.getE1ementById("i2").value;
+   var z = x * y;
+
+  if (z > 100) {
+       document.getE1elementById("res").style.color = "red"; 
+       alert(Muito caro!");
+} else {     
+       document.getE1ementById("res").style.color = "red";
+} else { 
+       document.getE1lementById("res").style.color = "green";
+}
+
+   document.getE1ementById("res").innerHTML = "Total: " + z;
+   console.log("calculado");
+} 
+
+
+//VERSÃO 2
+  const precoSement = 150.0;
+  funtion calcularInvestimentoTotal(qualidade) {
+     return qualidade * precoSemente;
+} 
+
+ funtion atualizarInterface(valorTotal) {
+    const display = document.getElementById("resultadoDisplay");
+    display.innertext = `O investimento total é: R$ ${valorTota.toFidex(2)}`;
+    display.style.color = valoTotal > 1000 ?  "orange" : "green";
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
